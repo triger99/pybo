39697292ee5d6992ce3d3837ba6210ab8c2cb50a
shcloar <ssmyym@ajou.ac.kr>
2022-05-24 00:42:56 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Question(models.Model):
+    autor = models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Question(models.Model):
+    subject = models.CharField(max_length = 200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
